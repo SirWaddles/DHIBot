839ed38be77114b507ccad1283f80ea4593e2a2c
James Wilkinson <jw@jameswilko.com>
2018-06-25 10:47:21 +0200

    Lets annoy spiffy for a bit

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ client.on('message', msg => {
         msg.reply('4'); // Chosen by fair dice roll - guranteed to be random.
         return;
     }
+    if ( msg.content.includes('dayz') ) {
+        msg.reply('bad game'); // lets annoy sylver and spiffy
+        return;
+    }
 });
 
 client.login(DiscordToken);
