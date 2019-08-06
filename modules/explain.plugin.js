65de9885a07dcfeb99d3794981f7876d49d37c70
Will Donohoe <will@will.io>
2019-08-05 21:29:08 -0400

    Explain things in an embed

--- modules/explain.plugin.js
+++ modules/explain.plugin.js
@@ -1,5 +1,6 @@
 import BaseModule from './module';
 import longestCommonSubstring from './lcs';
+import Discord from 'discord.js';
 
 class ExplainModule extends BaseModule {
     receiveMessage(msg) {
@@ -13,14 +14,16 @@ class ExplainModule extends BaseModule {
         // Get information on all the references
         const refs = this.db.getMarkovReferences(latestMsg.id);
 
-        // Build up a string with information on all the references
-        const lines = [];
+        // Build up an embed with information on all the references
+        let embed = new Discord.RichEmbed()
+            .setColor('#e54d42');
+
         for (const ref of refs) {
             const substring = longestCommonSubstring(ref.content, latestMsg.content);
-            lines.push(`* \`${substring}\` from ${ref.username} in <#${ref.channelID}>: https://discordapp.com/channels/${ref.guildID}/${ref.channelID}/${ref.messageID}`);
+            embed = embed.addField(substring, `[${ref.username.replace('[', '').replace(']', '')}](https://discordapp.com/channels/${ref.guildID}/${ref.channelID}/${ref.messageID})`);
         }
 
-        msg.reply('\n' + lines.join('\n'));
+        msg.channel.send(embed);
     }
 
     testMessage(msg) {
