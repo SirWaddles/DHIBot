d2824c2e6d68b7cd285de21e1a04eebdb59a3a46
James Wilkinson <jw@jameswilko.com>
2019-08-27 22:37:56 +0200

    very important plugin

---
+++ modules/september.plugin.js
@@ -0,0 +1,24 @@
+import BaseModule from './module';
+
+class SeptemberModule extends BaseModule {
+    receiveMessage(msg) {
+        msg.channel.send(
+            "https://www.youtube.com/watch?v=Gs069dndIYk"
+        );
+
+        this.can_post = false;
+
+        const self = this;
+        var nextInHours = Math.random() * (18 - 8) + 8;
+        setInterval(() => {
+        	self.can_post = true;
+        }, 1000 * 60 * 60 * nextInHours);
+    }
+
+    testMessage(msg) {
+    	var today = new Date();
+        return this.can_post && today.getMonth() == 8;
+    }
+}
+
+export default SeptemberModule;
